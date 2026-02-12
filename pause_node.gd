7616ee7259ccccd30555db6b9eed677e154acac7
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-02-12 21:49:42 +0500

    menu fixes

--- pause_node.gd
+++ pause_node.gd
@@ -1,9 +1,5 @@
 extends CanvasLayer
 
-func _ready() -> void:
-	if OS.get_name() == "Android" || OS.get_name() == "iOS":
-		$"Virtual Joystick".visible = true
-
 var isPaused = false
 
 func switchPause():
@@ -11,27 +7,28 @@ func switchPause():
 		isPaused = false
 		$PauseBloor.visible = false
 		$PauseCnt.visible = false
+		if OS.get_name() == "Android" || OS.get_name() == "iOS":
+			$JumpButton.visible = true
+			$"Virtual Joystick".visible = true
+			$MenuButton.visible = true
 		get_tree().paused = false
+		return
 	else:
 		isPaused = true
 		$PauseBloor.visible = true
 		$PauseCnt.visible = true
+		if OS.get_name() == "Android" || OS.get_name() == "iOS":
+			$JumpButton.visible = false
+			$"Virtual Joystick".visible = false
+			$MenuButton.visible = false
 		get_tree().paused = true
-	
-
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("pause"):
+		return
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
 		switchPause()
 
-func _on_to_main_pressed() -> void:
-	isPaused = false
-	$PauseBloor.visible = false
-	$PauseCnt.visible = false
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://menu.tscn")
-	
 func _on_cancel_main_pressed() -> void:
-	isPaused = false	
-	$PauseBloor.visible = false
-	$PauseCnt.visible = false
-	get_tree().paused = false
+	switchPause()
+func _on_to_main_pressed() -> void:
+	switchPause()
+	get_tree().change_scene_to_file("res://menu.tscn")
